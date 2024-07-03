80082dc5c84cc929d85df775aa64e6c08afd4dcc
Freddyd95 <freddydiaz28@hotmail.com>
2024-07-02 22:45:27 -0700

    added final changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import CustomNavBar from './Components/Navbar';
+import BackgroundImageForm from './Components/images';
+import StoreListings from './Components/StoreListing';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div>
+      <CustomNavBar />
+      <Container>
+        <BackgroundImageForm />
+        <StoreListings />
+      </Container>
     </div>
-  );
+  )
 }
 
 export default App;
--- src/Components/StoreListing.js
+++ src/Components/StoreListing.js
@@ -1,22 +1,39 @@
-import React from "react";
-import { Card } from 'react-bootstrap';
+import React from 'react';
+import { Row, Col, Card } from 'react-bootstrap';
 
-const BackgroundImageForm = () => {
-    return (
-        <Card className="bg-green text-white">
-            <Card.Img src="path_to_instacart_background_image" alt="Background Image" />
-            <Card.ImgOverLay>
-                <Card.Title>Find Stores Near You</Card.Title>
-                <Card.Text>
-                    Enter your address to start shopping at your favorite local stores.
-                </Card.Text>
-                <Card.Text>
-                    <input type="text" placeholder="Enter your address"/>
-                    <button type="submit">Submit</button>
-                </Card.Text>
-            </Card.ImgOverLay>
-        </Card>
+const StoreListings = () => {
+    return(
+        <Row>
+            <Col xs={12} sm={4}>
+                <Card>
+                    <Card.Img src="path_to_store_logo_1" alt="Store Logo"/>
+                    <Card.Body>
+                        <Card.Title>Store 1</Card.Title>
+                        <Card.Text>Store description goes here.</Card.Text>
+                    </Card.Body>
+                </Card>
+            </Col>
+            <Col xs={12} sm={4}>
+                <Card>
+                    <Card.Img src="path_to_store_logo_2" alt="Store Logo"/>
+                    <Card.Body>
+                        <Card.Title>Store 2</Card.Title>
+                        <Card.Text>Store description goes here.</Card.Text>
+                    </Card.Body>
+                </Card>
+            </Col>
+            <Col xs={12} sm={4}>
+                <Card>
+                    <Card.Img src="path_to_store_logo_3" alt="Store Logo"/>
+                    <Card.Body>
+                        <Card.Title>Store 3</Card.Title>
+                        <Card.Text>Store description goes here.</Card.Text>
+                    </Card.Body>
+                </Card>
+            </Col>
+            {/*Additional listings can be added similarly */}
+        </Row>
     )
 }
 
-export default BackgroundImageForm;
+export default StoreListings;
